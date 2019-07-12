f99cf8f958b5586bb2daa41e80bb96bf418b1d06
Clark Williams <theclarkwilliams@gmail.com>
2019-07-12 10:30:20 -0600

    axios in place, everything is rendering now

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -6,16 +6,16 @@ const StarWarsChar = props => {
             <h2>{props.charName}</h2>
             <p>{props.birthYear}</p>
             <p>{props.eyeColor}</p>
-            <p>{props.films}</p>
+            {/* <p>{props.films}</p> */}
             <p>{props.gender}</p>
             <p>{props.hairColor}</p>
             <p>{props.height}</p>
-            <p>{props.homeworld}</p>
+            {/* <p>{props.homeworld}</p> */}
             <p>{props.mass}</p>
             <p>{props.skinColor}</p>
-            <p>{props.species}</p>
-            <p>{props.starships}</p>
-            <p>{props.vehicles}</p>
+            {/* <p>{props.species}</p> */}
+            {/* <p>{props.starships}</p> */}
+            {/* <p>{props.vehicles}</p> */}
         </div>
     );
 };
