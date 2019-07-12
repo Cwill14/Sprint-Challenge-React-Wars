5acec7a216d10595204f427f900a290d9672f125
Clark Williams <theclarkwilliams@gmail.com>
2019-07-12 10:17:53 -0600

    first commit, got StarWarsChar and StarWarsList set up

---
+++ starwars/src/components/StarWarsChar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const StarWarsChar = props => {
+    return (
+        <div>
+            <h2>{props.charName}</h2>
+            <p>{props.birthYear}</p>
+            <p>{props.eyeColor}</p>
+            <p>{props.films}</p>
+            <p>{props.gender}</p>
+            <p>{props.hairColor}</p>
+            <p>{props.height}</p>
+            <p>{props.homeworld}</p>
+            <p>{props.mass}</p>
+            <p>{props.skinColor}</p>
+            <p>{props.species}</p>
+            <p>{props.starships}</p>
+            <p>{props.vehicles}</p>
+        </div>
+    );
+};
+
+export default StarWarsChar;
