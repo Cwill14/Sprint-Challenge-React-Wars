84fa69a1400316a261dd1ba4d5d41c4f1a9a53a2
Clark Williams <theclarkwilliams@gmail.com>
2019-07-12 11:09:10 -0600

    used styled components. now going to add tests

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
 import StarWarsList from './components/StarWarsList';
+import './App.css';
+// import styled from 'styled-components';
+
+// const Loading = styled.h3`
+//   color: white;
+// `;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [list, setList] = useState([])
+  const [list, setList] = useState([]);
+  // const [error, setError] = useState({});
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -18,13 +24,31 @@ const App = () => {
         console.log(res.data.results)
         setList(res.data.results)
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        // setError(err)
+      })
   }, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {/* { list !== [] ? 
+        <StarWarsList charList={list} /> :
+        <Loading>Loading ...</Loading>      
+      } */}
+      {/* { list === [] ? 
+        <Loading>Loading ...</Loading> :
+        <StarWarsList charList={list} />
+      } */}
       <StarWarsList charList={list} />
+      {/* {error ? 
+        <div className="error">
+          <h2>Oopsies! You have an error!</h2>
+          <h3>{error}</h3> 
+        </div> :
+        null    
+      } */}
     </div>
   );
 }
--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CharContainer = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: #aaa;
+    margin: 1rem auto;
+    padding: 0.75rem;
+    border-radius: 1.5rem;
+    h3 {
+        color: white;
+    }
+`;
+// width: 90%;
+
+const CharTrait = styled.p`
+    margin: 0 0.25rem;
+`;
+// color: white;
+
 
 const StarWarsChar = props => {
     return (
-        <div>
-            <h2>{props.charName}</h2>
-            <p>{props.birthYear}</p>
-            <p>{props.eyeColor}</p>
-            {/* <p>{props.films}</p> */}
-            <p>{props.gender}</p>
-            <p>{props.hairColor}</p>
-            <p>{props.height}</p>
-            {/* <p>{props.homeworld}</p> */}
-            <p>{props.mass}</p>
-            <p>{props.skinColor}</p>
-            {/* <p>{props.species}</p> */}
-            {/* <p>{props.starships}</p> */}
-            {/* <p>{props.vehicles}</p> */}
-        </div>
+        <CharContainer>
+            <h3>{props.charName}  </h3>
+            <CharTrait><b>Birth Year:</b> {props.birthYear}</CharTrait>
+            <CharTrait><b>Eye Color:</b> {props.eyeColor}</CharTrait>
+            {/* <CharTraits>{props.films}</CharTraits> */}
+            <CharTrait><b>Gender:</b> {props.gender}</CharTrait>
+            <CharTrait><b>Hair Color:</b> {props.hairColor}</CharTrait>
+            <CharTrait><b>Height:</b> {props.height}</CharTrait>
+            {/* <CharTrait>{props.homeworld}</CharTrait> */}
+            <CharTrait><b>Mass:</b> {props.mass}</CharTrait>
+            <CharTrait><b>Skin Color:</b> {props.skinColor}</CharTrait>
+            {/* <CharTrait>{props.species}</CharTrait> */}
+            {/* <CharTrait>{props.starships}</CharTrait> */}
+            {/* <CharTrait>{props.vehicles}</CharTrait> */}
+        </CharContainer>
     );
 };
 
